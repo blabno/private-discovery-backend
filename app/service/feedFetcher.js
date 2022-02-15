bea6e1cd41a3e819b37c15065a70caa2b5254bc2
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2022-02-15 08:31:40 +0000

    PM-35 GUID is bad identifier

--- app/service/feedFetcher.js
+++ app/service/feedFetcher.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const $http = require('http-as-promised');
 const moment = require('moment');
 const xml2js = require('xml2js');
+const uuid = require('uuid');
 
 const { sha256 } = require('./crypto');
 const wallItemDAO = require('../dao/wallItemDAO');
@@ -16,7 +17,12 @@ const parseRSS = async body => {
   return _.chain(result)
     .get('rss.channel[0].item')
     .map(i => mapFields({ title: 'title.0', link: 'link.0', pubDate: 'pubDate.0', guid: 'guid[0]._' }, i))
-    .map(({ title, link, pubDate, guid }) => ({ title, link, createDate: moment(pubDate).valueOf(), id: guid }))
+    .map(({ title, link, pubDate, guid }) => ({
+      title,
+      link,
+      createDate: moment(pubDate).valueOf(),
+      id: guid || uuid.v4()
+    }))
     .value();
 };
 
@@ -36,7 +42,9 @@ const fetchFeed = async ({ id, url, format }) => {
   // TODO pass the lastRead argument
   const body = await $http.get(url, { resolve: 'body' });
   const items = await parse(format, body);
-  return wallItemDAO.createBulk(mapIds(id, items));
+  if (items.length) {
+    return wallItemDAO.createBulk(mapIds(id, items));
+  }
 };
 
 module.exports = {
